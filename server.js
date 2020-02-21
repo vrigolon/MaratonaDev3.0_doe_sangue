28b3b83bdabf38029ff0f1a4a252ba725b7e0025
vitor.rigolon <vrigolon@mail.com>
2020-02-21 15:25:23 +0000

    add conection to database

--- server.js
+++ server.js
@@ -1,11 +1,27 @@
 const express = require("express")
 const server = express()
 
+
+//Config servidor para apresentar arquivos estaticos
 server.use(express.static('public'))
 
+
+//habilita body do formulario
 server.use(express.urlencoded({ extended: true }))
 
 
+//configurar conexao com banco de dados
+const Pool = require('pg').Pool
+const db = new Pool({
+  user: 'postgres',
+  password: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  database: 'doe'
+})
+
+
+//configura template engine
 const nunjucks = require("nunjucks")
 nunjucks.configure("./", {
   express: server,
@@ -13,40 +29,42 @@ nunjucks.configure("./", {
 })
 
 
-const donors = [
-  {
-    name: "Joaquim José",
-    blood: "B+"
-  },
-  {
-    name: "Jose Almeida",
-    blood: "A+"
-  },
-  {
-    name: "Josenildo João",
-    blood: "AB+"
-  },
-  {
-    name: "Josefino José",
-    blood: "O+"
-  },
-]
-
+//configurar a apresentaçao da pagina
 server.get("/", function(req, res) {
+
+  db.query("SELECT * FROM donors", function(err, result) {
+    if (err) return res.send("Erro de banco de dados")
+  const donors = result.rows
   return res.render("index.html", { donors })
+  })
 })
 
+
 server.post("/", function(req, res) {
+  //pegar dados do formulario
   const name = req.body.name 
   const email = req.body.email 
   const blood = req.body.blood
 
-  donors.push({
-    name: name,
-    blood: blood,
-  })
+  if (name == "" || email == "" || blood == "") {
+    return res.send("Todos os campos são obrigatórios.")
+  }
 
-  return res.redirect("/")
+  
+  //colocar valores dentro do banco de dados
+  const query = `
+  INSERT INTO donors ("name", "email", "blood")
+  VALUES ($1, $2, $3)`
+
+  const values = [name, email, blood]
+
+  db.query(query, values, function(err) {
+    if (err) return res.send("erro no banco de dados")
+    return res.redirect("/")
+
+    })
+
+  
 })
 
 server.listen(3000)
